0a8a352f65c4241c2942a9c4f91dba0496d7d95d
Alejandro Ortiz <117711330+amoozing2539@users.noreply.github.com>
2025-10-16 17:15:57 -0700

    Syntax and spelling errors
    ALMOST works

--- source/DetectorConstruction.cc
+++ source/DetectorConstruction.cc
@@ -24,7 +24,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 	// World
 	G4Box* solidWorld = new G4Box("World", 1.0 * m, 1.0 * m, 1.0 * m); 
 	G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, world_mat, "logicWorld");
-	G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "physWorld"); //place world centered at (0,0,0)
+	G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(), logicWorld, "physWorld", 0, false, 0, true); //place world centered at (0,0,0)
 
 	// Foil
 	G4double foil_thickness = 2.0 * mm;
@@ -36,10 +36,10 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 	// Sensitive Detector
 	G4double detector_thickness = .1 * mm;
 	G4double detector_xy = 30.0 * cm;
-	G4ThreeVector dtector_pos = G4ThreeVector(0, 0, (0.5 * foil_thickness) + (0.5 * detector_thickness) + (1.0 * mm)); //as close to the foil as possible (1mm).
+	G4ThreeVector detector_pos = G4ThreeVector(0, 0, (0.5 * foil_thickness) + (0.5 * detector_thickness) + (1.0 * mm)); //as close to the foil as possible (1mm).
 	G4Box* solidDetector = new G4Box("Detector", 0.5 * detector_xy, 0.5 * detector_xy, 0.5 * detector_thickness);
 	G4LogicalVolume* logicDetector = new G4LogicalVolume(solidDetector, detector_mat, "logicalDetector");
-	new G4PVPlacement(0, detector_pos, logicDetecotr, "physDetector", logicWorld, false, 0, true);
+	new G4PVPlacement(0, detector_pos, logicDetector, "physDetector", logicWorld, false, 0, true);
 
 	// make detector volume a sensitive detector
 	auto aSensitiveDetector = new SensitiveDetector("SensitiveDetector");
